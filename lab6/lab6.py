5f0aee9d054d107b78c8d741fd629051130647cd
vivian19-meet <vivian19@meet.mit.edu>
2017-12-12 18:48:52 +0200

    “COMMENT”done

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,5 +1,6 @@
 from turtle import *
 import random
+import math
 class Ball(Turtle):
 	def __init__(self, radius, color, speed):
 		Turtle.__init__(self)
@@ -10,7 +11,30 @@ class Ball(Turtle):
 		self.speed(speed)
 circle_1 = Ball(9,"blue",45)
 
-circle_2 = Ball(12,"blue",32)
-def check_collision(cicle_1,circle_2):
-	if math.sqrt(math.pow((circle_2.xcor()-circle_1.xcor(),2) + math.pow((circle_2.ycor()-circle_1.ycor(),2))<=circle_1.radius+circle_2.radius
-		print("nice!")
+circle_2 = Ball(12,"red",32)
+circle_3 = Ball(20,"green",75)
+circle_4 = Ball(39,"yellow",63)
+def check_collision(circle_1,circle_2):
+	h = math.sqrt(math.pow(circle_2.xcor()-circle_1.xcor(),2) + math.pow((circle_2.ycor()-circle_1.ycor()),2))
+	y = circle_1.radius+circle_2.radius
+	if h <= y:
+		return True
+	else: 
+		return False
+#check_collision(circle_1,circle_2)
+vv = [circle_1,circle_2,circle_3,circle_4]
+def check_list(list1):
+	for i in range(len(vv)):
+		for j in range(i+1,len(vv)):
+			if check_collision(vv[i],vv[j]) == True :
+				x = random.randint(-500, 500)
+				y = random.randint(-500, 500)
+				if vv[i].radius > vv[j].radius:
+					vv[j].goto(x,y)
+				else:
+					vv[i].goto(x,y)
+
+check_list(vv)
+
+
+mainloop()
